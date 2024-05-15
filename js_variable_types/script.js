055fcba0c56c1d65ea627b44ffddb7edca98bfcb
unknown <pickerwork@gmail.com>
2024-05-15 04:55:28 +0300

    Variable Types

---
+++ js_variable_types/script.js
@@ -0,0 +1,12 @@
+let productName = "Iphone 13";
+let productPrice = 15000;
+
+console.log(typeof productName);
+console.log(typeof productPrice);
+
+let number1 = 10;
+let number2 = 20;
+
+//Number() .toString
+console.log(number1 + number2)
+
