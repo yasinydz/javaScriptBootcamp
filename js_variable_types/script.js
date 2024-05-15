031803b6f1d937653bcc337980ae024b76f574e3
unknown <pickerwork@gmail.com>
2024-05-15 04:56:09 +0300

    Variable Types typeof

--- js_variable_types/script.js
+++ js_variable_types/script.js
@@ -10,3 +10,14 @@ let number2 = 20;
 //Number() .toString
 console.log(number1 + number2)
 
+let name = "Yasin";
+let surname = "Yıldız";
+
+console.log(name + " " + surname);
+
+let examNote = 70;
+let isSuccessful = (examNote >= 50);
+
+console.log(examNote);
+console.log(typeof examNote);
+
