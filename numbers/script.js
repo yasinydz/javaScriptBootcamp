a5bee4a4aa4b6064b5faca02fc1755a63ee3999d
unknown <pickerwork@gmail.com>
2024-05-16 07:53:00 +0300

    Math numbers

---
+++ numbers/script.js
@@ -0,0 +1,26 @@
+let result;
+
+result = Number("10");
+result = parseInt("10.6");
+result = parseFloat("10.6");
+result = parseInt("a10"); //NaN
+
+result = isNaN("10");
+
+let number = 15.5344331;
+
+result = number.toPrecision(5);
+result = number.toFixed(5);
+
+result = Math.round(2.4);
+result = Math.ceil(2.2);
+result = Math.floor(2.2);
+result = Math.sqrt(25);
+result = Math.pow(2,3);
+result = Math.abs(-3);
+result = Math.min(4,6,4,3,4,9);
+result = Math.max(4,6,4,3,4,9);
+result = Math.random() * 10 + 1;
+
+
+
