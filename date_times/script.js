ec61cf9f8013fedfb8403f054807967d4bd661a3
unknown <pickerwork@gmail.com>
2024-05-16 08:05:52 +0300

    date

---
+++ date_times/script.js
@@ -0,0 +1,21 @@
+let now = new Date();
+
+//get
+result = now.getFullYear();
+result = now.getMonth();
+result = now.getDate();
+result = now.getDay();
+result = now.getHours();
+result = now.getMinutes();
+result = now.getMilliseconds();
+result = now.getTime();
+
+//set
+now.setFullYear(2025);
+now.setMonth(7);
+
+result = now;
+
+console.log(result);
+
+
