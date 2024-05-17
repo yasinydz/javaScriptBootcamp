3f593258083d0bec411b35b28b09504083f0a963
unknown <pickerwork@gmail.com>
2024-05-18 01:58:43 +0300

    array

---
+++ array/script.js
@@ -0,0 +1,21 @@
+const products = [
+    {
+        productName : "Iphone 11",
+        price : 9000,
+        phoneColor : "Gold"
+    },
+    {
+        productName : "Iphone 12",
+        price : 11000,
+        phoneColor : "Black"
+    },
+    {
+        productName : "Iphone 13",
+        price : 12000,
+        phoneColor : "White"
+    }
+]
+
+for (const product of products) {
+    console.log(product)
+}
